8174c74a8265f2cb44b1bad4d414f8ad12a37aa1
KaitlynRossi <kaitirossi@gmail.com>
2025-04-23 16:48:07 -0400

    Checkout, Policies, and Order Confirmation Aesthetic Changes

--- obj/Debug/net8.0/ASPProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/ASPProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ada0d848c4b6c7e58ced6fe6b24c71a053eb258")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3883167438486d9f0cc1c0fb893b3e32597e59c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
